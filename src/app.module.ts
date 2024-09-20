09cb6b5c075849a4ccafa0901318579eaa5c57e4
Luis henrique pavão <82249306+devLuiss@users.noreply.github.com>
2024-09-19 23:34:42 -0400

    commit save

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { CreateAccountController } from "src/controllers/create-account.controller";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Module({
+  imports: [],
+  controllers: [CreateAccountController],
+  providers: [PrismaService],
+})
+export class AppModule {}
---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,59 @@
+import {
+  Body,
+  ConflictException,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+} from "@nestjs/common";
+import { hash } from "bcryptjs";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type CreateAccountBody = z.infer<typeof createAccountBodySchema>;
+
+@Controller("/accounts")
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBody) {
+    const { name, email, password } = body;
+
+    const emailAlreadyExists = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (emailAlreadyExists) {
+      throw new ConflictException("Email already exists");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return {
+      message: "Account created",
+    };
+  }
+
+  @Get()
+  async list() {
+    return this.prisma.user.findMany();
+  }
+}
